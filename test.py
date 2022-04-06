16ff458562a2a6f209b7c8c54033b0ff58efaeb0
HelpMeCodePls <64028614+HelpMeCodePls@users.noreply.github.com>
2022-04-06 21:21:08 +0800

    Update test.py

--- test.py
+++ test.py
@@ -66,7 +66,7 @@ def load_modal():
 def run_app(p_path, c_path, e_path):
 
     load_modal()
-    opt = TestOptions.parse()
+    opt = TestOptions().parse()
     opt.warp_checkpoint = MODEL_W_WEIGHTS
     opt.gen_ceckpoint = MODEL_G_WEIGHTS
 
