9932937f005726270dec0dbf03a7363b5960b7a3
HelpMeCodePls <64028614+HelpMeCodePls@users.noreply.github.com>
2022-04-06 21:24:47 +0800

    Update base_options.py

--- options/base_options.py
+++ options/base_options.py
@@ -46,7 +46,7 @@ class BaseOptions():
             if id >= 0:
                 self.opt.gpu_ids.append(id)
 
-        if len(self.opt.gpu_ids) > 0:
+#        if len(self.opt.gpu_ids) > 0:
 #            torch.cuda.set_device(self.opt.gpu_ids[0])
 
         args = vars(self.opt)
