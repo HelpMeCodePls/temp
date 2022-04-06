0d8b8040034b680dc2e0f00a506f8b73dac81761
HelpMeCodePls <64028614+HelpMeCodePls@users.noreply.github.com>
2022-04-06 18:53:04 +0800

    Update test.py

--- test.py
+++ test.py
@@ -214,7 +214,7 @@ def main():
 
     app_mode = st.sidebar.selectbox("Please Select", SIDEBAR_OPTIONS)
     if app_mode == SIDEBAR_OPTION_DEMO_IMAGE:
-        st.sidebar.write("Slect Demo Images")
+        # st.sidebar.write("Slect Demo Images")
 
         directory_img = os.path.join(DEFAULT_DATA_BASE_DIR, IMAGE_DIR_IMG)
         directory_clo = os.path.join(DEFAULT_DATA_BASE_DIR, IMAGE_DIR_CLOTHES)
